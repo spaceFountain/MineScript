fba45caa92332c7db7c5599975adaf0b8afcdf52
spaceFountain <spacefountains@gmail.com>
2015-02-18 00:25:45 -0500

    Began to implement the Bukkit code.

--- src/vg/civcraft/mc/MineScript/MineScriptPlugin.java
+++ src/vg/civcraft/mc/MineScript/MineScriptPlugin.java
@@ -1,9 +1,22 @@
 package vg.civcraft.mc.MineScript;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import vg.civcraft.mc.MineScript.computer.ComputerManager;
+
+import java.util.logging.Logger;
 
 /**
  * Created by isaac on 2/8/15.
  */
 public class MineScriptPlugin extends JavaPlugin{
+    public static MineScriptPlugin self;
+    public static Logger logger;
+    private ComputerManager computerManager;
+
+    @Override
+    public void onEnable() {
+        self = this;
+        logger = this.getLogger();
+        computerManager = new ComputerManager();
+    }
 }
